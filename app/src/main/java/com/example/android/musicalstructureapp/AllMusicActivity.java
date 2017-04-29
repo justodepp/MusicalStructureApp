bf804b0818b4a9da1f46b825a92747b3eb336a1c
justodeeper <gianniandreacavalli@gmail.com>
2017-04-29 18:23:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/musicalstructureapp/AllMusicActivity.java
@@ -0,0 +1,114 @@
+package com.example.android.musicalstructureapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by justo on 29/04/17.
+ */
+
+public class AllMusicActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_allmusic);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Find the View that shows the numbers category
+        TextView genre = (TextView) findViewById(R.id.genre);
+
+        // Set a click listener on that View
+        genre.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent genreIntent = new Intent(AllMusicActivity.this, GenreActivity.class);
+
+                // Start the new activity
+                startActivity(genreIntent);
+            }
+        });
+
+        // Find the View that shows the numbers category
+        TextView album = (TextView) findViewById(R.id.album);
+
+        // Set a click listener on that View
+        album.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent albumIntent = new Intent(AllMusicActivity.this, AlbumActivity.class);
+
+                // Start the new activity
+                startActivity(albumIntent);
+            }
+        });
+
+        // Find the View that shows the numbers category
+        TextView artist = (TextView) findViewById(R.id.artist);
+
+        // Set a click listener on that View
+        artist.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent artistIntent = new Intent(AllMusicActivity.this, ArtistActivity.class);
+
+                // Start the new activity
+                startActivity(artistIntent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.goHome) {
+            // Handle the home action
+        } else if (id == R.id.allMusic) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
