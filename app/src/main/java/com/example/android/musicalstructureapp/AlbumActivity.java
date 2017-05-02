86b97201ac0123f65f9641e8b3ce718651ba596e
Gianni <gianniandreacavalli@gmail.com>
2017-05-02 15:53:46 +0200

    Initial commit

--- app/src/main/java/com/example/android/musicalstructureapp/AlbumActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/AlbumActivity.java
@@ -37,12 +37,7 @@ public class AlbumActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+        super.onBackPressed();
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
